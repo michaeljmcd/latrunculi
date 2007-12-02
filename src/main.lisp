b3807a299fecc5b8f9a408ab057dd9cb4c6223da
Michael McDermott <michaeljmcd@users.noreply.github.com>
2007-12-02 06:36:32 +0000

    Continued work on porting main.lisp over to Common Lisp. Merging initialize.lisp with main.lisp as there is little code in either.

--- src/main.lisp
+++ src/main.lisp
@@ -19,7 +19,7 @@
    (ai-settings :accessor get-ai-settings))
   )
 
-(define player0 (list BLACK 
+(defparameter player0 (list BLACK 
 		      "Ajax" 
 		      AI 
 		      '((3 . (0 . 0)) (3 . (1 . 0)) (3 . (2 . 0))
@@ -31,7 +31,7 @@
 		      '#(40 95 65 0.0001 0.0001)
 		      ))
 
-(define player1 (list WHITE 
+(defparameter player1 (list WHITE 
 		      "Achilles" 
 		      HUMAN 
 		      '((2 . (0 . 11)) (2 . (1 . 11)) (2 . (2 . 11))
@@ -49,6 +49,51 @@
 ; that the player has and their positions, and A is the player's AI settings (ignored
 ; if under player control). A is of the form:
 ; #(search-depth own-pawn-value opponent-pawn-value own-king-mobility-value opponent-king-mobility-value)
+
+(require 'vector-lib)
+
+(defconstant COLS 12)
+(defconstant ROWS 8)
+
+(defconstant EMPTY -1)
+(defconstant WHITE_KING 0)
+(defconstant WHITE_PAWN 2)
+(defconstant BLACK_KING 1)
+(defconstant BLACK_PAWN 3)
+
+(define create-game-board (lambda ()
+			    '#(#(3 3 3 3 3 3 3 3 3 3 3 3)
+			       #(-1 -1 -1 -1 -1 1 -1 -1 -1 -1 -1 -1)
+			       #(-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
+			       #(-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
+			       #(-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
+			       #(-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1)
+			       #(-1 -1 -1 -1 -1 -1 0 -1 -1 -1 -1 -1)
+			       #(2 2 2 2 2 2 2 2 2 2 2 2)
+			       )
+			    ))
+; Returns a freshly allocated copy of the default game board.
+; The board will be represented as a vector of vectors.
+
+; create-game-board returns a list of lists representing the 12 x 8 board. 
+; The board, 
+; internally, will have the following representation: a list of lists, where 
+; each list is a row. The list elements will be tuples having the 
+; following legend:
+;-1 - Empty 
+; 0 - White King
+; 1 - Black King
+; 2 - White 'pawn'
+; 3 - Black 'pawn'
+;
+; There is a reason for this. After doing this any white piece mod 2 = 0
+; and any black piece mod 2 = 1. This provides a quick way to determine 
+; color.
+;
+; A tuple will be of the form (L . I) where L is a value from the above
+; legend and I is the ID for the piece if applicable and -1 if N/A. 
+
+; ROWS, COLS used as CONSTANTS! These define the dimensions of the board.
 
 (initialize-display)
 ;(show-menu)
