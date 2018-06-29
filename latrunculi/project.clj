fe182018480cd407d0c23bf0d7f35dee9ce4ef59
Michael McDermott <michael.mcdermott@jbhunt.com>
2018-06-29 15:56:14 -0500

    Ported some metadata from latrunculi.asd.

--- latrunculi/project.clj
+++ latrunculi/project.clj
@@ -1,8 +1,8 @@
 (defproject latrunculi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "The game of Latrunculi"
+  :url "https://github.com/michaeljmcd/latrunculi"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot latrunculi.core
   :target-path "target/%s"
