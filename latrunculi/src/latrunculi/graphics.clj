e71b76b502e9055e0b0b97e1ae86130273916eb7
Michael McDermott <michael.mcdermott@jbhunt.com>
2018-07-02 12:42:38 -0500

    Moved libraries to a location more indicative of their usage; ported more initialization logic from the original.

--- latrunculi/src/latrunculi/graphics.clj
+++ latrunculi/src/latrunculi/graphics.clj
@@ -1,14 +1,40 @@
 (ns latrunculi.graphics
- (:import (org.lwjgl.glfw GLFW)))
+ (:import (org.lwjgl.glfw GLFW)
+          (org.lwjgl.opengl GL GL11)
+          (org.lwjgl BufferUtils)))
+
+(def global-state (atom 0))
 
 (defn start []
- (GLFW/glfwInit)
+ (assert (GLFW/glfwInit) "Failed to initialize GLFW.")
  (GLFW/glfwDefaultWindowHints)
  (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
  (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_FALSE)
  (GLFW/glfwWindowHint GLFW/GLFW_SAMPLES 4)
 
- (let [window (GLFW/glfwCreateWindow (int 1024) (int 768) "ASDF" (long 0) (long 0))]
+ (let [window (GLFW/glfwCreateWindow (int 1024) (int 768) "Latrunculi" (long 0) (long 0))]
+  (GLFW/glfwSetCursor window  (GLFW/glfwCreateStandardCursor GLFW/GLFW_CROSSHAIR_CURSOR))
+  (GLFW/glfwMakeContextCurrent window)
+  (GLFW/glfwSwapInterval 0)
   (GLFW/glfwShowWindow window)
+
+  (let [intBuffer (BufferUtils/createIntBuffer 2)]
+   (GL/createCapabilities)
+
+   ; Initialize OpenGL
+   (GL11/glEnableClientState GL11/GL_VERTEX_ARRAY)
+
+   (GL11/glEnable GL11/GL_TEXTURE_2D)
+   (GL11/glClearDepth 1.0)
+   (GL11/glEnable GL11/GL_DEPTH_TEST)
+   (GL11/glShadeModel GL11/GL_SMOOTH)
+   (GL11/glEnable GL11/GL_BLEND)
+   (GL11/glHint GL11/GL_PERSPECTIVE_CORRECTION_HINT GL11/GL_NICEST)
+   (GL11/glBlendFunc GL11/GL_SRC_ALPHA GL11/GL_ONE_MINUS_SRC_ALPHA)
+  )
+
+  (Thread/sleep (long 5000))
+
+  (GLFW/glfwSetWindowShouldClose window true)
  )
 )
