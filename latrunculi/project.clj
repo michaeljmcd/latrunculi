e724c6b4b0e78b939df679c4ea484773f324f628
Michael McDermott <michael.mcdermott@jbhunt.com>
2018-07-02 10:29:14 -0500

    Setting up graphics module. Added Windows native DLLs to lib (will need to do something for Linux.

--- latrunculi/project.clj
+++ latrunculi/project.clj
@@ -8,6 +8,7 @@
        [org.lwjgl/lwjgl "3.1.6"]
        [org.lwjgl/lwjgl-glfw "3.1.6"]
        [org.lwjgl/lwjgl-opengl "3.1.6"]]
+  :jvm-opts ["-Djava.library.path=lib"]
   :main ^:skip-aot latrunculi.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- latrunculi/src/latrunculi/graphics.clj
+++ latrunculi/src/latrunculi/graphics.clj
@@ -3,4 +3,12 @@
 
 (defn start []
  (GLFW/glfwInit)
+ (GLFW/glfwDefaultWindowHints)
+ (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
+ (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_FALSE)
+ (GLFW/glfwWindowHint GLFW/GLFW_SAMPLES 4)
+
+ (let [window (GLFW/glfwCreateWindow (int 1024) (int 768) "ASDF" (long 0) (long 0))]
+  (GLFW/glfwShowWindow window)
+ )
 )
